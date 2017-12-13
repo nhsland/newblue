3fc8ca86c2cd0fc3337ae3776cd9dc477a924088
Toby Fisher <toby@knowego.com>
2017-12-13 16:18:13 +0000

    added quicklook pop to clinic list

--- js/oe-clinic.js
+++ js/oe-clinic.js
@@ -72,6 +72,7 @@ var clinic = {
 		this.setupFilters(); 				
 		this.setDurationGraphics();	
 		this.showCurrentTime();
+		this.quicklook();
 	},	
 	
 	/**
@@ -821,6 +822,52 @@ var clinic = {
 		});	
 	},
 	
+	/**
+	quicklook popup
+	**/
+	quicklook:function(){
+		var $popup = $('#patient-clinic-quicklook');
+	
+		/**
+		Events	
+		**/
+		// quicklook and warning triangle are wrapped
+		// unclick to close
+		$('.js-clinic-quicklook').click(function( e ){
+			e.stopPropagation();
+			var pos = clinic.getPosition( $(this) );
+			showQuicklook( $(this), pos.top );
+		});
+		
+		$popup.click(function( e ){
+			e.stopPropagation();
+			hideQuicklook();
+		});
+		
+		function showQuicklook( $wrapper, top ){
+			offsetTop = top - 30;
+			$popup.find('.patient-details .name').text( $wrapper.data('name') );
+			$popup.find('.patient-details .number').text( $wrapper.data('id') );
+			
+			// show alert?
+			if( $wrapper.find('.triangle').length ){
+				$popup.find('.alert-box').show();
+			} else {
+				$popup.find('.alert-box').hide();
+			}
+			
+			$popup
+				.removeClass('hidden')
+				.css( {'top':offsetTop} )
+				.show();
+		}
+		
+		function hideQuicklook(){
+			$popup.hide();
+		}
+		
+	},
+	
 	/** 
 	UI setup DEMO (based on DOM elements)
 	**/
@@ -865,6 +912,14 @@ var clinic = {
 		tr.find('.clinic-assign-options').parent().text('Dr Amit Baum (AB)');
 		tr.find('.js-add-pathway').hide();
 		$('#filter-AB .total').text(1);
+		
+		/**
+		Flag some Risks / Allergies	
+		**/
+		var tr = $( '#patient-'+1008002 );
+		tr.find('.js-clinic-quicklook').append('<i class="oe-i triangle inline small">');
+		var tr = $( '#patient-'+ 1897143 );
+		tr.find('.js-clinic-quicklook').append('<i class="oe-i triangle inline small">');
 		
 	},
 };
