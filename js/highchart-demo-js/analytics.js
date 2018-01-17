ee3b804ad058ae89ded17fdc800eb8234ef3deed
Toby Fisher <toby@knowego.com>
2018-01-17 23:32:45 +0000

    Classic Theme reworked. Typography improved

--- js/highchart-demo-js/analytics.js
+++ js/highchart-demo-js/analytics.js
@@ -33,7 +33,9 @@ var optionsAnalytics = {
 	},
 	
 	yAxis: {
-		title:'Count', 
+		title: {
+			text: 'yAxis',
+		}, 
 		min:0,
 		max:100 
 	},
@@ -97,8 +99,10 @@ var optionsDiagnoses = {
 	},
 	
 	yAxis: {
-		title:'Patients'
-		
+		title: {
+			text: 'Patients',
+			
+		}
 	},
 	
 	legend: {
@@ -129,5 +133,71 @@ var optionsDiagnoses = {
         colorIndex:51,
     }]
 } 
+
+var optionsVA = {
+	chart: {
+		className: 'oes-chart-analytics',
+		height:'60%',					// aspect ratio to height
+		spacing: [15, 10, 15, 10], 		// then make space for title - default: [10, 10, 15, 10] 
+	},
+	
+	credits: { enabled: false },  // highcharts url (logo) removed
+	
+	title: {
+		text: '',
+		align: 'center',
+		margin:0,
+		y:0, 				// title needs offset to not go under legend in small mode
+	},
+	
+	xAxis: {
+		title: {
+			text: 'Months',			
+		},
+		
+		categories: ['Macular degeneration',' Diabetic Macular Oedema (bin)','BRVO','CRVO','Hemivein','Other','No Diagnoses'], 
+		
+		labels: {  
+			y:25				// move labels further below ticks
+		},
+	},
+	
+	yAxis: {
+		title: {
+			text: 'ETDRS',	
+		}
+		
+		min:55,
+		max:70
+	},
+	
+	legend: {
+		enabled: false
+	},
+
+
+	plotOptions: {
+        bar: {
+            animation: {
+				duration: 0, // disable the inital draw animation 
+        	}
+        },
+        series: {
+            cursor: 'pointer',
+            events: {
+                click: function (event) {
+                    showFakePatientDataList(this.chart.title.textStr, event.point.x, event.point.y);
+                }
+            }
+        }
+	},
+	
+	series: [{
+        name: 'Data',
+        type: 'bar',
+        data:[1256,1100,346,459,34,654,890],
+        colorIndex:51,
+    }]
+}
 
 	
