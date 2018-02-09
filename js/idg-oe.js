109c6b11cb3629a01100fd435160cb786c69f499
Toby Fisher <toby@knowego.com>
2018-02-09 22:14:25 +0000

    update to JS demo

--- js/idg-oe.js
+++ js/idg-oe.js
@@ -1315,9 +1315,12 @@ idg.auditTrail = function(){
 	
 	if( $('#js-event-audit-trail-btn').length == 0 ) return;
 		
+	var show = false	
+		
 	// loop through the view tiles and check the data height
 	$('#js-event-audit-trail-btn').click(function(){
 		$('#js-event-audit-trail').toggle();
+		$(this).toggleClass('active');
 	});
 	
 	
