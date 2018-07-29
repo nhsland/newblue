42f21bcb926b7b9d360c2d7b387c840a91762a71
Toby Fisher <toby@knowego.com>
2018-07-29 16:53:41 +0100

    Improvement: tooltip, offset left added

--- js/idg/ui/tooltips.js
+++ js/idg/ui/tooltips.js
@@ -6,10 +6,20 @@ idg.tooltips = function(){
 		function(){
 			var text = $(this).data('tooltip-content');
 			var offset = $(this).offset();
-			var leftPos = offset.left - 94 // tooltip is 200px (and center on the icon)
+			var leftPos, toolCSS; 
+			
+			// check for the available space for tooltip:
+			if ( ( $( window ).width() - offset.left) < 100 ){
+				leftPos = offset.left - 174 // tooltip is 200px (left offset on the icon)
+				toolCSS = "oe-tooltip offset-left";
+			} else {
+				leftPos = offset.left - 94 // tooltip is 200px (center on the icon)
+				toolCSS = "oe-tooltip";
+			}
+			
 			// add, calculate height then show (remove 'hidden')
 			var tip = $( "<div></div>", {
-								"class": "oe-tooltip",
+								"class": toolCSS,
 								"style":"position:fixed; left:"+leftPos+"px; top:0;"
 								});
 			// add the tip:
