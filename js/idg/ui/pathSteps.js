de205984c95cd2cb994376f756e5c9f43654d248
Toby Fisher <toby@knowego.com>
2019-01-12 11:21:38 +0000

    Update: pathstep

--- js/idg/ui/pathSteps.js
+++ js/idg/ui/pathSteps.js
@@ -63,7 +63,9 @@ idg.pathSteps = {
 	*/
 	CreatePopup:function(){
 		
-		// build Data DOM
+		/*
+		build Data DOM
+		*/
 		const $div 		= $('<div class="pathstep-popup"></div>');	
 	 	const $close 	= $('<div class="close-icon-btn"><i class="oe-i remove-circle medium"></i></div>');	
 	 	const $status 	= $('<div class="step-status">status</div>');
@@ -72,18 +74,49 @@ idg.pathSteps = {
 		const $dataGroup = $('<div class="data-group"></div>');
 		const $dataList = $('<ul class="data-list"></ul>');
 		
-		// data DOM
+		// pin confirmation
+		const $pin		= $('<div class="pin-confirmation"></div>');
+		const $pinRight = $('<div class="eye-confirmation right"><h3>Right</h3></div>');
+		const $pinLeft = $('<div class="eye-confirmation left"><h3>Left</h3></div>');
+		const $inputPinRight = inputPIN('right');
+		const $inputPinLeft = inputPIN('left');
+	
+		function inputPIN( side ){
+			return $('<input id="pathstep-user-pin-'+side+'" type="text" maxlength="4" inputmode="numeric" placeholder="****">');
+		}
+		
+		// edit PSD
+		const $edit 	= $('<div class="step-actions"><button class="blue hint">Edit PSD</button></div>').hide();
+		
+		/*
+		compile DOM
+		*/
+
+		// dataList DOM
 		$dataGroup.append( $dataList );
+		
+		// pin
+		$pinRight.append( $inputPinRight );
+		$pinLeft.append( $inputPinLeft );
+		$pin.append( $pinRight, $pinLeft ).hide();
 				
 		// build DOM element, and hide it
 		$div.append(	$close, 
 						$status,
 						$title, 
 						$eye, 
-						$dataGroup ).hide();
+						$dataGroup,
+						$pin,
+						$edit ).hide();
 
 		// attach to DOM
 		$('body').append( $div );
+		
+		function resetDOM(){
+			$status.removeClass();
+			$edit.hide();
+			$pin.hide();
+		}
 		
 		/*
 		Methods	
@@ -113,7 +146,19 @@ idg.pathSteps = {
 			switch( stepData.status ){
 				case "done":
 					$status.text('Done: 09:20');
-					$status.addClass('green');
+					$status.addClass('step-status green');
+				break;
+				case "todo":
+					$status.text('Waiting');
+					$status.addClass('step-status orange');
+					$edit.show();
+					$pin.show();
+				break;
+				case "problem":
+					$status.text('Problem! e.g. Patient has left');
+					$status.addClass('step-status red');
+					$edit.show();
+					$pin.show();
 				break;
 				
 				default: $status.text('no status');
@@ -123,15 +168,43 @@ idg.pathSteps = {
 			// now show it
 			$div.show();
 		}
+		
+		this.pinRightChange = function( e ){
+			checkPin( e.target.value, $pinRight, "right");
+		}
+		
+		this.pinLeftChange = function( e ){
+			checkPin( e.target.value, $pinLeft, "left");
+		}
+		
+		function checkPin( pin, $side, side ){
+			if( pin.length === 4){
+				if(pin === "1234"){
+					$side.addClass("correct-pin");
+				} else {
+					$side.addClass("wrong-pin");
+				}
+			} else {
+				$side.removeClass("wrong-pin correct-pin");
+			}
+			
+		}
 	
 		this.close = function(){
 			$div.hide();
+			resetDOM();
 		}
+		
+		
 
 		/*
 		Events
 		*/
 		$close[0].addEventListener( "click", this.close.bind( this ) );
+		$inputPinRight[0].addEventListener("input", this.pinRightChange.bind( this ));
+		$inputPinLeft[0].addEventListener("input", this.pinLeftChange.bind( this ));
+		
+		
 	}
 	
 }
