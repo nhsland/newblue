b005c4d5c16b6b5d3ed9fc0dd44986d46fba96c4
Toby Fisher <toby@knowego.com>
2019-02-20 11:16:33 +0000

    New Eyedraw icons

--- js/idg/patient/patientPopups.js
+++ js/idg/patient/patientPopups.js
@@ -13,9 +13,10 @@ idg.patientPopups = {
 		var demographics 	= new idg.PatientBtnPopup( 'demographics', $('#js-demographics-btn'), $('#patient-popup-demographics') );
 		var demographics2 	= new idg.PatientBtnPopup( 'management', $('#js-management-btn'), $('#patient-popup-management') );
 		var risks 			= new idg.PatientBtnPopup( 'risks', $('#js-allergies-risks-btn'), $('#patient-popup-allergies-risks') );
+		var chart			= new idg.PatientBtnPopup( 'charts', $('#js-charts-btn'), $('#patient-popup-charts') );
 	
 	
-		var all = [ quicklook, demographics, demographics2, risks ];
+		var all = [ quicklook, demographics, demographics2, risks, chart ];
 		
 		for( pBtns in all ) {
 			all[pBtns].inGroup( this ); // register group with PopupBtn 
