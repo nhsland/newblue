54a2b1f1806ef7763ed2e0be01460d3f0978e55d
Toby Fisher <toby@knowego.com>
2019-11-26 10:17:56 +0000

    Updating to Gulp 4, improving newblue build structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,258 @@
-/**
-OE v3.0 
-Gulp file generates:
-- style_oe3.0.css 
-- style_oe3.0_classic.css : 'classic' colour scheme
-CSS is readable and compressed.	
-- eyedraw_draw_icons.css : Eyedraw doodle icon
-- eyedraw-draw-icons-32x32.png : Sprite sheet of Eyedraw doodle icons
-JS files for IDG v3 demos
-- oe-ui.js
-- oe-clinic.js
-- oe-oescape
-**/
+/*
+OE UI - Gulp generates:
+- style_oe3.0.css  					// Pro/Dark (expanded)
+- style_oe3.0.min.css				// Pro/Dark (minified) 
+- style_oe3.0_classic.min.css		// Classic/Light (minified)
+- style_oe3,0_print.css				// Print (minified)
+- eyedraw_draw_icons.min.css		// Eyedraw doodle icons (minified)
+- eyedraw_draw_icons-32x32.png		// Eyedraw icon spritesheet
+- event-icons-76x76.png				// Event icons spritesheet
+ */
 
-var gulp 			= require('gulp');
-// sass
-var sass 			= require('gulp-sass');
-var autoprefixer		= require('gulp-autoprefixer');
-// sprites
-var spritesmith 		= require('gulp.spritesmith');
-// js
-var concat       	= require('gulp-concat');
-var uglify       	= require('gulp-uglify'); 
-// var pump 			= require('pump');
-// files
-var rename 			= require('gulp-rename');
-var es 				= require('event-stream');
-// headers
-var header 			= require('gulp-header');
-
-
-/**
-Config
-- sprite config var in functions
-- don't use relative paths if you want to watch new or deleted files
-**/ 
-var config = {
-	sass:			'./sass/style_oe3.0_pro.scss',					// scss for Pro (dark) OE v3.0  
-	classic:			'./sass/style_oe3.0_classic.scss', 				// scss for Classic (light) theme
-	print:			'./sass/style_oe3.0_print.scss',					// print
-	eyedrawSass:		'./sass/style_eyedraw-draw-icons.scss', 			// eyedraw draw doodle icons
-	cssFile:			'style_oe3.0',									// suffixed 
-	css:				'./css',
-	watchSass:		'./sass/**/*.scss',								// watch scss												
-	idgJS: 			'../idg-php/idgJS/idg/**/*.js',					// ---- watch IDG demo JS files
+const config = {
+	version: '4.3',
+	css: 'style_oe',
+	reload: false,
 }
 
-/**
-default 
-- Build CSS for Pro and Classic themes
-- Build IDG demo JS 
-- watch...
-**/
-gulp.task('default',['sass','sass-classic','sass-print','readJS','watch-build']);
+const paths = {
+	src: {
+		path: 		'./src/sass/',
+		pro: 		'./src/sass/style_oe_pro.scss',
+		classic: 	'./src/sass/style_oe_classic.scss',
+		print: 		'./src/sass/style_oe_print.scss',
+		watch: 		'./src/sass/**/*.{scss,sass}',
+	},
+	dist: {
+		css: 		'./css/',
+	},
+};
+
+/*
+Packages
+*/
+const {gulp, src, dest, watch, series, parallel} = require('gulp');
+const del = require('del');
+const flatmap = require('gulp-flatmap');
+const lazypipe = require('lazypipe');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+// styles
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const prefix = require('autoprefixer');
+const minify = require('cssnano');			// options for CSS Nano: https://cssnano.co/guides/optimisations/
+// SVGs
+const svgmin = require('gulp-svgmin');
+// sprites
+const spritesmith = require('gulp.spritesmith');
+// BrowserSync
+const browserSync = require('browser-sync');
+// Fix for file modifications datetime
+const through2 = require('through2');
+
+/*
+- Datestamp (for debug)
+- Legal header for CSS files
+*/
+var headerDateStamp = () => '/* ' + new Date( Date.now() ) + ' */';
+var headerLegals = [	'/**',
+					'*  (C) OpenEyes Foundation, 2018 ',
+					'*  This file is part of OpenEyes. ',
+					'*',
+					'* OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.',
+					'* OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.',
+					'* You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.',
+					'*',
+					'* @link http://www.openeyes.org.uk',
+					'*',
+					'* @author OpenEyes <info@openeyes.org.uk>',
+					'* @copyright Copyright (C) 2017, OpenEyes Foundation',
+					'* @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0',
+					'*',
+					'*/',
+					'',''].join('\n');
+
+/*
+-----------------------------
+Process and minify SASS files
+-----------------------------
+*/
+
+/*
+PRO (dark) theme
+- Expanded CSS file for Debug / Development
+- Compressed CSS file for deployment
+*/
+var proCSS = function (done) {
+	// return stream (async task)
+	// so task manager knows it's done 
+	return src(paths.src.pro)
+		.pipe(sass({
+			outputStyle: 'expanded',
+			sourceComments: false,		// add sass line and file reference for debug?
+			errLogToConsole:true,
+		}).on( 'error', sass.logError ))
+		.pipe(postcss([
+			prefix({					// autoprefix CSS
+				cascade: true,
+				remove: true
+			})
+		]))
+		.pipe(header( '\n/* Expanded CSS for debugging and development purposes only - use minified version for live site */\n' ))
+		.pipe(header( headerDateStamp() ))
+		.pipe(rename( config.css + config.version + '.css'))
+		.pipe( through2.obj( function( file, enc, cb ) {
+			// fixes a 'feature' in Gulp 4 that stops
+			// the file modified date from updating correctly...
+			var date = new Date();
+			file.stat.atime = date;
+			file.stat.mtime = date;
+			cb( null, file );
+		}) )
+		.pipe(dest(paths.dist.css))
+		.pipe(postcss([
+			minify({
+				discardComments: { removeAll: true },
+				normalizeWhitespace:true,
+				cssDeclarationSorter:true,
+				uniqueSelectors:true,
+				orderedValues:true,
+				discardEmpty:true,
+			})
+		]))
+		.pipe(header( headerDateStamp() ))
+		.pipe(header( headerLegals ))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(dest(paths.dist.css));
+};
+
+/*
+CLASSIC (light) theme
+- Compressed CSS file for deployment
+*/
+var classicCSS = function (done) {
+	return src(paths.src.classic)
+		.pipe(sass({
+			outputStyle: 'expanded',
+		}))
+		.pipe(postcss([
+			prefix({					
+				cascade: true,
+				remove: true
+			})
+		]))
+		.pipe( through2.obj( function( file, enc, cb ) {
+			var date = new Date();
+			file.stat.atime = date;
+			file.stat.mtime = date;
+			cb( null, file );
+		}) )
+		.pipe(postcss([
+			minify({
+				discardComments: { removeAll: true },
+				normalizeWhitespace:true,
+				cssDeclarationSorter:true,
+				uniqueSelectors:true,
+				orderedValues:true,
+				discardEmpty:true,
+			})
+		]))
+		.pipe(header( headerDateStamp() ))
+		.pipe(header( headerLegals ))
+		.pipe(rename( config.css + config.version + '_classic.min.css'))
+		.pipe(dest(paths.dist.css));
+};
+
+/*
+PRINT
+- Compressed CSS file for deployment 
+*/
+var printCSS = function (done) {
+	return src(paths.src.print)
+		.pipe(sass({
+			outputStyle: 'expanded',
+		}))
+		.pipe(postcss([
+			prefix({					
+				cascade: true,
+				remove: true
+			})
+		]))
+		.pipe( through2.obj( function( file, enc, cb ) {
+			var date = new Date();
+			file.stat.atime = date;
+			file.stat.mtime = date;
+			cb( null, file );
+		}) )
+		.pipe(postcss([
+			minify({
+				discardComments: { removeAll: true },
+				normalizeWhitespace:true,
+				cssDeclarationSorter:true,
+				uniqueSelectors:true,
+				orderedValues:true,
+				discardEmpty:true,
+			})
+		]))
+		.pipe(header( headerDateStamp() ))
+		.pipe(header( headerLegals ))
+		.pipe(rename( config.css + config.version + '_print.css'))
+		.pipe(dest(paths.dist.css));
+};
+
 
 /**
-watch scss (Pro & Classic themes)	
+Eyedraw sprite PNG and SCSS
+- creates the PNG sprite sheet for Eyedraw
+- creates a SCSS sprite positioning file 
 **/
-gulp.task('watch-build', function() {
-    gulp.watch( config.watchSass , ['sass','sass-classic','sass-print']);
-    gulp.watch( config.idgJS , ['readJS'] ); // IDG demo JS files
-});
+
+
+/**
+Eyedraw Draw CSS file
+- requires the 'eyedraw-draw-sprites.scss', which
+is generated by task 'eyedraw_sprites' below
+- creates readable and compressed css	
+**/
+
+
+
+/**
+Event sprite PNG and SCSS
+- creates the PNG sprite sheet for Events
+- creates a SCSS sprite positioning file
+**/
+
+
+// ********************************************
+
+// Reload the browser when files change
+var reloadBrowser = function (done) {
+	console.log('hi');
+	//if (!config.reload) return done();
+	//browserSync.reload();
+	done();
+};
+
+var watchCSS = function(done){
+	watch(paths.src.watch, series(exports.buildCSS,reloadBrowser));
+	done();
+}
+
+/*
+--------------
+Export tasks
+'gulp --tasks'
+--------------
+*/
+
+// Build all OE CSS files: "gulp buildCSS"
+exports.buildCSS = parallel( proCSS, classicCSS, printCSS );
+
+// Default task: "gulp"
+exports.default = series( exports.buildCSS, watchCSS );
+
 
 /**
 Eyedraw icons
@@ -74,210 +267,7 @@ Build spritesheet & scss for Event Icons, rarely updated, run this task, then de
 2) Run default 
 **/
 
-/*
-DateStamp files to allow for comparisson between IDG & OE development
-*/
-var cssDateStamp = function(){
-	return '/* ' + new Date( Date.now() ) + ' */';
-}
 
-var oeLegals = function(){
-return 		[	'/**',
-				'*  (C) OpenEyes Foundation, 2018 ',
-				'*  This file is part of OpenEyes. ',
-				'*',
-				'* OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.',
-				'* OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.',
-				'* You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.',
-				'*',
-				'* @link http://www.openeyes.org.uk',
-				'*',
-				'* @author OpenEyes <info@openeyes.org.uk>',
-				'* @copyright Copyright (C) 2017, OpenEyes Foundation',
-				'* @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0',
-				'*',
-				'*/',
-				'',''].join('\n');
-}
-
-/**
-PRO theme (v3.0) 
-create readable and compressed css	
-**/
-gulp.task('sass',function(){
-		
-	var min = gulp.src( config.sass )
-				.pipe( sass( {outputStyle:'compressed'} ) )
-				.pipe( rename( config.cssFile + '.min.css') )
-				.pipe( autoprefixer()) // browserList set in package.json
-				.pipe( header( cssDateStamp() ) )
-				.pipe( header( oeLegals() ) );
-				
-					
-	var css = gulp.src( config.sass )
-				.pipe( sass( {errLogToConsole:true, outputStyle:'expanded'} ).on( 'error', sass.logError ) )
-				.pipe(rename( config.cssFile + '.css') )
-				.pipe( autoprefixer() ) // browserList set in package.json
-				.pipe( header( oeLegals() ) );
-					
-	// merge and output			
-	return	es.merge( min,css ).pipe( gulp.dest( config.css ) );
-});
-
-/**
-Classic theme (v3.0) 
-only creates compressed css 
-(as identical to PRO apart from colour changes)
-**/
-gulp.task('sass-classic',function(){
-	
-	return gulp.src( config.classic )
-		.pipe( sass( {outputStyle:'compressed'} ) )
-		.pipe( rename( config.cssFile + '_classic.min.css') )
-		.pipe( autoprefixer() ) 
-		.pipe( header( cssDateStamp() ) )
-		.pipe( header( oeLegals() ) )				
-		.pipe( gulp.dest( config.css ) );
-});
-
-/**
-Print (v3.0) 
-only creates compressed css 
-**/
-gulp.task('sass-print',function(){
-	
-	return gulp.src( config.print )
-		.pipe( sass( {outputStyle:'compressed'} ) )
-		.pipe( rename( config.cssFile + '_print.css') )
-		.pipe( autoprefixer() ) 
-		.pipe( header( cssDateStamp() ) )				
-		.pipe( gulp.dest( config.css ) );
-});
-
-
-/**
-Eyedraw sprite PNG and SCSS
-- creates the PNG sprite sheet for Eyedraw
-- creates a SCSS sprite positioning file 
-**/
-gulp.task('eyedraw_sprites', function () {
-	// ensure the path is correct to files or it will fail silently.
-	// ALL icons MUST be 32 x 32 px!!
-	// Because they are used at 2 sizes: normal and 50% in CSS (.small)
-	var spriteIcons = 'img/icons-eyedraw/32x32/**/*.png';
-	
-	var gulpScss = [	'//',
-						'// --- Gulp Generated SCSS file! ',
-						'// --- Do not edit this file directly ',
-						'//',
-						'// --- OE Eyedraw Icons - sprite positions',
-						'//',
-						''].join('\n'); 	
-
-	var spriteData = gulp.src( spriteIcons )
-						.pipe(spritesmith({
-								imgName: 		'../img/eyedraw-draw-icons-32x32.png', 				// export PNG sprite sheet
-								cssName: 		'_eyedraw-draw-sprites.scss',						// create SASS sprite sheet 
-								cssTemplate: 	'sass/_handlebars/eyedrawSprite.scss.handlebars', 	// SASS template file
-								cssHandlebarsHelpers: { half: function (num) { return num/2; } }
-								
-					}));
-					
-   // img
-   spriteData.img.pipe(gulp.dest( 'img' ));
-   // sass
-   spriteData.css.pipe( header( gulpScss) );
-   spriteData.css.pipe(gulp.dest( 'sass/eyedraw-icons' ));
-  
-});
-
-/**
-Eyedraw Draw CSS file
-- requires the 'eyedraw-draw-sprites.scss', which
-is generated by task 'eyedraw_sprites' below
-- creates readable and compressed css	
-**/
-gulp.task('eyedraw_sass',function(){
-	
-	var min = gulp.src( config.eyedrawSass )
-				.pipe( sass( {outputStyle:'compressed'} ) )
-				.pipe( rename('eyedraw_draw_icons.min.css') )
-					
-	var css = gulp.src( config.eyedrawSass )
-				.pipe( sass( {errLogToConsole:true, outputStyle:'expanded'} ).on( 'error', sass.logError ) )
-				.pipe( rename('eyedraw_draw_icons.css') )
-					
-	// merge and output			
-	return	es.merge( min,css ).pipe( gulp.dest( config.css ) );
-});
-
-
-/**
-Event sprite PNG and SCSS
-- creates the PNG sprite sheet for Events
-- creates a SCSS sprite positioning file
-**/
-gulp.task('event_sprites', function () {
-	// ensure the path is correct to files or it will fail silently.
-	// ALL icons MUST be 76px x 76px
-	// Used by CSS at 50% and at 25% (38px & 19px)
-	
-	var spriteIcons = 'img/icons-events/76x76/*.png';
-	
-	var gulpScss = ['//',
-  '// --- Gulp Generated SCSS file ',
-  '// --- Do not edit this file directly ',
-  '//',
-  '// --- OE Event Icons - sprite positions',
-  '//',
-  ''].join('\n');  	
-
-	var spriteData = gulp.src( spriteIcons )
-						.pipe(spritesmith({
-								imgName: 		'../img/event-icons-76x76.png', 					// export PNG sprite sheet
-								cssName: 		'_icons-events-sprites.scss',						// create SCSS sprite sheet 
-								cssTemplate: 	'sass/_handlebars/eventSprite.scss.handlebars', 	// SCSS template file
-								cssHandlebarsHelpers: { 	half: function (num) { return num/2; },
-															quarter: function (num) { return num/4; }  }
-								
-					}));
-   
-   spriteData.img.pipe(gulp.dest( 'img' ));
-   
-   spriteData.css.pipe( header( gulpScss) );
-   spriteData.css.pipe(gulp.dest( 'sass/openeyes' ));
-  
-});
-
-// ********************************************
-
-/**
-IDG demo JS
-Using to gulp to handle all the JS for IDG demos
-This is NOT meant for production.
-**/
-gulp.task( 'readJS', function() {	
-	gulp.src( config.idgJS )
-	.pipe( concat( 'idg-oe.js' ) )
-	.pipe( gulp.dest( '../idg-php/idgJS' ) );
-});
-
-/**
-Use Pump to get readable Ugilfy errors
-**/
-
-/*
-gulp.task('uglyJS', function (cb) {
-  pump([
-      gulp.src( config.idgJS ),
-      concat( 'idg-oe.min.js' ),
-      uglify(),
-      gulp.dest('./js')
-    ],
-    cb
-  );
-});
-*/
 
 
 
